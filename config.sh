c1285f5680fb721cee685c4385f749411b60f68a
vagrant <vagrant@vagrant-centos65.vagrantup.com>
2014-09-01 13:18:59 +0900

    tokyoリージョンを使用するように変更

--- config.sh
+++ config.sh
@@ -3,9 +3,9 @@
 aws_account="daito"
 aws_key="aws_${aws_account}_key"
 aws_security_group="aws_${aws_account}_security_group"
-aws_region="us-west-2c"
-aws_spot_price="0.009"
+aws_region="ap-northeast-1"
+aws_spot_price="0.011"
 
-aws_image_id="ami-d13845e1"
+aws_image_id="ami-047bca05"
 aws_instance_type_t2_small="t2.small"
 aws_instance_type_m3_medium="m3.medium"
