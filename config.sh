28db9228025479254e218f22ad3d33c928fbb6d9
genzouw <genzouw@gmail.com>
2014-09-03 01:30:00 +0900

    ec2関連のコマンドラインツールラッパ追加

--- config.sh
+++ config.sh
@@ -1,10 +1,18 @@
 #!/bin/bash -u
 
-aws_account="daito"
+aws_account="genzouw"
 aws_key="aws_${aws_account}_key"
 aws_security_group="aws_${aws_account}_security_group"
-aws_region="us-west-2"
+aws_region="us-west-2c"
 
-aws_image_id="ami-d13845e1"
-aws_instance_type_t2_small="t2.small"
+# amazon linux
+# aws_spot_price="0.011"
+# aws_image_id="ami-f45beff5"
+
+# windows
+aws_image_id="ami-73cd8843"
+aws_spot_price="0.08"
+
 aws_instance_type_m3_medium="m3.medium"
+
+# [ ! -d .config/ ] && mkdir -p .config/
